31f0a5d1c1099c99bb4d8d7615539132faac9d46
Haocheng <yuanhaocheng910505@gmail.com>
2019-12-22 16:10:34 +1100

    Updates

---
+++ precache-manifest.02e9591871ba8473f0c26d8581ca30cc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df682315fe50ade1667336af640e1847",
+    "url": "/weatherapp_k2tech/index.html"
+  },
+  {
+    "revision": "d814631583ba95487f6f",
+    "url": "/weatherapp_k2tech/static/css/2.01ccd7bc.chunk.css"
+  },
+  {
+    "revision": "e267432da704e704b6d4",
+    "url": "/weatherapp_k2tech/static/css/main.e80d953d.chunk.css"
+  },
+  {
+    "revision": "d814631583ba95487f6f",
+    "url": "/weatherapp_k2tech/static/js/2.defe6a79.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weatherapp_k2tech/static/js/2.defe6a79.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e267432da704e704b6d4",
+    "url": "/weatherapp_k2tech/static/js/main.4fd44306.chunk.js"
+  },
+  {
+    "revision": "341effa11ceea65d013d",
+    "url": "/weatherapp_k2tech/static/js/runtime-main.f53bc516.js"
+  },
+  {
+    "revision": "38a1d465a3a02c91700f661df17db396",
+    "url": "/weatherapp_k2tech/static/media/sydney-destination.38a1d465.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/weatherapp_k2tech/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/weatherapp_k2tech/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/weatherapp_k2tech/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/weatherapp_k2tech/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/weatherapp_k2tech/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
